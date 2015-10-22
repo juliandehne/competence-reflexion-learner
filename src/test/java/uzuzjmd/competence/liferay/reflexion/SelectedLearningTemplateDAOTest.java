68bdb64777a48ba446f35dec05e1cc50c905f9c4
julian.dehne@gmail.com <uzuzjmd12>
2015-10-22 13:02:03 +0200

    slightly changed test so he reflects the semantic of the tested interface
    the setup now creates the required user and course context

--- src/test/java/uzuzjmd/competence/liferay/reflexion/SelectedLearningTemplateDAOTest.java
+++ src/test/java/uzuzjmd/competence/liferay/reflexion/SelectedLearningTemplateDAOTest.java
@@ -15,33 +15,47 @@ import com.sun.jersey.api.client.filter.LoggingFilter;
 
 public class SelectedLearningTemplateDAOTest {
 
+	private final static String user = "xunguyen";
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		Client client = com.sun.jersey.api.client.Client.create();
+		client.addFilter(new LoggingFilter(System.out));
+		WebResource webResource = client.resource("http://localhost:8084"
+				+ "/competences/json/user/create/" + user + "/teacher");
+
+		try {
+			webResource.queryParam("groupId", "user")
+					.accept(MediaType.APPLICATION_JSON).post();
+		} finally {
+			client.destroy();
+		}
 
 	}
 
 	@AfterClass
 	public static void tearDown() {
-		
+
 	}
-	
+
+	/**
+	 * tests that interface does not through exception may not return data if
+	 * user has not previously selected learning templates for himself to learn
+	 */
 	@Test
-	public void testFindAll() {
+	public void testSelectedTemplates() {
 		Client client = com.sun.jersey.api.client.Client.create();
 		client.addFilter(new LoggingFilter(System.out));
 		WebResource webResource = client.resource("http://localhost:8084"
 				+ "/competences/xml/learningtemplates/selected");
-		
-		System.out.println("FETCHING FROM_: "+webResource.getURI());
+
+		System.out.println("FETCHING FROM_: " + webResource.getURI());
 
 		StringList result = null;
-		String user = "xunguyen";
-		
+
 		try {
 
-			result = webResource
-					.queryParam("userId",
-							 user + "")
+			result = webResource.queryParam("userId", user + "")
 					.queryParam("groupId", "user")
 					.accept(MediaType.APPLICATION_XML).get(StringList.class);
 		} finally {
